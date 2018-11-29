e80930fba9d9b5126485c571b63bc4b80ba65195
Zeswen <zeswentyrael@gmail.com>
2018-11-29 20:32:15 +0100

    Fixed stars

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
 const hbs          = require('hbs');
 const moment       = require('moment');
 const mongoose     = require('mongoose');
@@ -48,7 +47,6 @@ app.use(require('node-sass-middleware')({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 hbs.registerPartials(`${__dirname}/views/partials`);
 
 
