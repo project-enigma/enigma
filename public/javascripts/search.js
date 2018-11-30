8a59a53880398c5177e9ad7ec1fbb7ab7eab3822
DavidMunozvi <43704932+DavidMunozvi@users.noreply.github.com>
2018-11-30 11:11:01 +0100

    Feature/dmv/trips (#16)
    Debugging

---
+++ public/javascripts/search.js
@@ -0,0 +1,17 @@
+function searchFunction() {
+  let input; var filter; var ul; var li; var a; var 
+i;
+  input = document.getElementById('myinput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById('wrapper');
+  li = ul.getElementsByTagName('li');
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName('a')[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = '';
+    } else {
+      li[i].style.display = 'none';
+    }
+  }
+}
